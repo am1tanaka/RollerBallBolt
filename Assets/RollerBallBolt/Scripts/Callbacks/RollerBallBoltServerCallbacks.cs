11f9c97a11e9061208d182abc26fbb482935c309
Yu Tanaka <YRK00337@nifty.com>
2018-11-27 22:13:55 +0900

    プレイヤーとボールを、配置済みの場所に差し替え

---
+++ Assets/RollerBallBolt/Scripts/Callbacks/RollerBallBoltServerCallbacks.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using RollerBallBolt;
+
+[BoltGlobalBehaviour(BoltNetworkModes.Server, "Game")]
+public class TutorialServerCallbacks : Bolt.GlobalEventListener
+{
+    /// <summary>
+    /// このプログラムがシーンを読み込んだ時の処理
+    /// </summary>
+    /// <param name="map"></param>
+    public override void SceneLoadLocalDone(string map)
+    {
+        // プレイヤー1を生成して、操作を担当します
+        BoltEntity be = BoltNetwork.Instantiate(BoltPrefabs.Player);
+        be.transform.position = NetworkSceneManager.Instance.GetPlayerPosition(be.prefabId.Value);
+        be.TakeControl();
+
+        // ボールを作ります
+        be = BoltNetwork.Instantiate(BoltPrefabs.Ball);
+        be.transform.position = NetworkSceneManager.Instance.ballPosition.position;
+    }
+
+    /// <summary>
+    /// クライアントがシーンを読み込んだ時に報告されるコールバック
+    /// リモート用のプレイヤーを生成
+    /// </summary>
+    /// <param name="connection"></param>
+    public override void SceneLoadRemoteDone(BoltConnection connection)
+    {
+        // プレイヤー2を生成して、操作を接続先に任せます
+        BoltEntity be = BoltNetwork.Instantiate(BoltPrefabs.Player2);
+        be.transform.position = NetworkSceneManager.Instance.GetPlayerPosition(be.prefabId.Value);
+        be.AssignControl(connection);
+    }
+}
---
+++ Assets/RollerBallBolt/Scripts/NetworkSceneManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RollerBallBolt
+{
+    public struct OBJECT_RECORD
+    {
+        public int id;
+        public Vector3 position;
+    }
+
+    public class NetworkSceneManager : MonoBehaviour
+    {
+        public static NetworkSceneManager Instance
+        {
+            get;
+            private set;
+        }
+
+        public List<OBJECT_RECORD> playerPositions {
+            get;
+            private set;
+        }
+        public OBJECT_RECORD ballPosition
+        {
+            get;
+            private set;
+        }
+
+        private void Awake()
+        {
+            Instance = this;
+
+            playerPositions = new List<OBJECT_RECORD>();
+
+            GameObject[] gos = GameObject.FindGameObjectsWithTag("Player");
+            OBJECT_RECORD or;
+            foreach(GameObject go in gos)
+            {
+                or.id = go.GetComponent<BoltEntity>().prefabId.Value;
+                or.position = go.transform.position;
+                playerPositions.Add(or);
+                Destroy(go);
+            }
+
+            GameObject ball = GameObject.FindGameObjectWithTag("Ball");
+            or.id = ball.GetComponent<BoltEntity>().prefabId.Value;
+            or.position = ball.transform.position;
+            ballPosition = or;
+            Destroy(ball);
+        }
+
+        /// <summary>
+        /// BoltEntity.prefabId.Valueを受け取って、該当する
+        /// プレハブIDで記録された座標を返します。
+        /// </summary>
+        /// <param name="id">プレハブID</param>
+        /// <returns>座標を返します</returns>
+        public Vector3 GetPlayerPosition(int id)
+        {
+            foreach(OBJECT_RECORD or in playerPositions)
+            {
+                if (or.id == id)
+                {
+                    return or.position;
+                }
+            }
+            return Vector3.zero;
+        }
+    }
+}
